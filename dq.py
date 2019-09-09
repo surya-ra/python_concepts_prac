e7e9248d790cdab19bb68d83174cbb3c87095f4d
surya-ra <ess.dee3107@gmail.com>
2019-09-08 23:52:04 -0700

    commit for update of moving average code with deque() implementation

--- dq.py
+++ dq.py
@@ -1,18 +1,31 @@
-class TreeNode:
-	def __init__(self, key):
-		self.val = key
-		self.left = None
-		self.right = None
+from collections import deque
+import itertools
 
-class CheckFormat:
-	def acceptInput(self, root : TreeNode):
-		print(vars(root.left.left))
+class CheckDq:
+	def movingAverage(self, iterable, n):
+
+		elems_total = deque(iterable)
+		result = []
+
+		def logicImplement(local_elems_total, i):
+
+			it = iter(local_elems_total)
+			elems_sliced = deque(itertools.islice(it, n))
+			avg_value = sum(elems_sliced)/i
+			result.append(avg_value)
+			visited_points = elems_sliced.popleft()
+			local_elems_total.popleft()
+
+			if len(local_elems_total) != i-1 :
+				logicImplement(local_elems_total, i)
+
+		logicImplement(elems_total, n)
+		return result
+
 
 if __name__ == '__main__':
-	node = TreeNode(2)
-	node.left = TreeNode(5)
-	node.left.left = TreeNode(15)
-	node.right = TreeNode(4)
-	Caller = CheckFormat()
-	Caller.acceptInput(node)
-
+	CallClass = CheckDq()
+	# moving_average([40, 30, 50, 46, 39, 44]) --> 40.0 42.0 45.0 43.0)
+	val = int(input())
+	it_list = [40, 30, 50, 46, 39, 44]
+	print(CallClass.movingAverage(it_list, val))
